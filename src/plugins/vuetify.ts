6d69c9fb3abe2c3ad86db75361f22d31a3919812
nas11ai <kairupannasai@gmail.com>
2025-02-18 13:01:42 +0800

    init theme color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,19 +1,38 @@
-/**
- * plugins/vuetify.ts
- *
- * Framework documentation: https://vuetifyjs.com`
- */
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import 'vuetify/styles'
 
-// Styles
-import "@mdi/font/css/materialdesignicons.css";
-import "vuetify/styles";
+const myTheme = {
+  dark: false, // Set ke true jika ingin dark mode
+  colors: {
+    primary: '#FBBC04',   // Yellow (Main)
+    secondary: '#4285F4', // Blue (Accent)
+    error: '#EA4335',     // Red (Error)
+    success: '#34A853',   // Green (Success)
+    background: '#FFFFFF', // White Background
+    surface: '#F1F3F4',    // Light Gray Surface
+    noteGreen: '#E6F4EA',
+    notePink: '#FCE8E6',
+    noteBlue: '#E8F0FE',
+    noteYellow: '#FEF7E0',
+    noteOrange: '#FDE7E3',
+    textPrimary: '#3C4043',  // Dark Gray Text
+  },
+}
 
-// Composables
-import { createVuetify } from "vuetify";
-
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    // defaultTheme: 'dark',
+    defaultTheme: 'myTheme',
+    themes: {
+      myTheme,
+    },
   },
-});
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
